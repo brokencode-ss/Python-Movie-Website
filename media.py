0292a1aadaeea14b7814eac385de0bc4da257f12
brokencode-ss <41676351+brokencode-ss@users.noreply.github.com>
2018-11-21 21:45:35 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,6 +1,17 @@
 import webbrowser
 
 class Movie():
+    
+    """This is a class that initializes 4 attributes and contains one function.
+    
+    Attributes:
+    
+    title(str): It is the movie title.
+    storyline(str): It is the movie summary.
+    poster_image_url(str): It is the URL of movie poster.
+    trailer_youtube_url(str): It is the URL of the movie's trailer.
+    """
+    
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title=movie_title
         self.storyline=movie_storyline
